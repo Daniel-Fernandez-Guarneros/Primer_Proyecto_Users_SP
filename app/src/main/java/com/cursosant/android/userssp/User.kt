b016739a5cfdec04e88ee0e6ebeea8609f367d0f
Daniel Fernandez Guarneros <danierse63@gmail.com>
2021-11-03 20:27:13 -0600

    010 Nombre completo desde data class

--- app/src/main/java/com/cursosant/android/userssp/User.kt
+++ app/src/main/java/com/cursosant/android/userssp/User.kt
@@ -12,4 +12,5 @@ package com.cursosant.android.userssp
  ***/
 data class User (val id: Long, var name: String, var lastName: String, var url: String) {
 
+    fun getFullName(): String = "$name $lastName"
 }
