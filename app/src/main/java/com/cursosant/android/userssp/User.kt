e0a54ffef30bfba7a312c15be3bc7e1a59c38176
Daniel Fernandez Guarneros <danierse63@gmail.com>
2021-11-03 18:57:14 -0600

    002 Diseño de RecyclerView y Data Class

---
+++ app/src/main/java/com/cursosant/android/userssp/User.kt
@@ -0,0 +1,15 @@
+package com.cursosant.android.userssp
+
+/****
+ * Proyecto: Fundamentos Kotlin
+ *
+ * Daniel Fernandez Guarneros
+ *
+ * 4°A Desarrollo de Software Multiplataforma
+ *
+ * Diseño para Apps
+ *
+ ***/
+data class User (val id: Long, var name: String, var lastName: String, var url: String) {
+
+}
