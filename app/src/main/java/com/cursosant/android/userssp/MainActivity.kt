640b0484ce1f3d3e51ac06e8a12b2ef1c85d18c9
Daniel Fernandez Guarneros <danierse63@gmail.com>
2021-11-06 17:13:52 -0600

    018 Dialog: Mejores prácticas

--- app/src/main/java/com/cursosant/android/userssp/MainActivity.kt
+++ app/src/main/java/com/cursosant/android/userssp/MainActivity.kt
@@ -27,7 +27,6 @@ class MainActivity : AppCompatActivity(), OnClickListener {
 
         val isFirstTime = preferences.getBoolean(getString(R.string.sp_first_time), true)
         Log.i("SP", "${getString(R.string.sp_first_time)} = $isFirstTime")
-        Log.i("SP", "${getString(R.string.sp_username)} = ${preferences.getString(getString(R.string.sp_username), "NA")}")
 
         if (isFirstTime) {
             val dialgoView = layoutInflater.inflate(R.layout.dialog_register, null)
@@ -43,9 +42,14 @@ class MainActivity : AppCompatActivity(), OnClickListener {
                         putString(getString(R.string.sp_username), username)
                             .apply()
                     }
+                    Toast.makeText(this, R.string.register_success, Toast.LENGTH_SHORT)
+                        .show()
                 })
                 .show()
 
+        } else {
+            val username = preferences.getString(getString(R.string.sp_username), getString(R.string.hint_username))
+            Toast.makeText(this, "Bienvenido $username", Toast.LENGTH_SHORT).show()
         }
 
         userAdapter = UserAdapter(getUsers(), this)
